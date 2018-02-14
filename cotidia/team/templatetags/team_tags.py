be85b5cda3556067591d12e8a5d8005443917545
Guillaume Piot <guillaume@cotidia.com>
2018-02-14 15:12:53 +0000

    Update tag name to be more consistent.

--- cotidia/team/templatetags/team_tags.py
+++ cotidia/team/templatetags/team_tags.py
@@ -9,6 +9,7 @@ register = template.Library()
 def get_departments():
     return Department.objects.all().order_by('order_id')
 
+
 @register.simple_tag
-def get_team_members():
+def get_members():
     return Member.objects.filter(active=True).order_by('order_id')
