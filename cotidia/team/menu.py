6c1727234c53bcf8f147a739ca7a693be7447055
Guillaume Piot <guillaume@cotidia.com>
2019-01-12 21:44:31 +0000

    Update menu.

--- cotidia/team/menu.py
+++ cotidia/team/menu.py
@@ -4,15 +4,23 @@ from django.urls import reverse
 def admin_menu(context):
     return [
         {
-            "text": "Members",
             "icon": "users",
-            "url": reverse("team-admin:member-list"),
-            "permissions": ["team.add_member", "team.change_member"],
-        }, {
-            "text": "Departments",
-            "icon": "building",
-            "url": reverse("team-admin:department-list"),
-            "permissions": ["team.add_department",
-                            "team.change_department"],
-        },
+            "text": "Team management",
+            "description": "Manage members and departments.",
+            "align_right": True,
+            "nav_items": [
+                {
+                    "text": "Members",
+                    "icon": "users",
+                    "url": reverse("team-admin:member-list"),
+                    "permissions": ["team.add_member", "team.change_member"],
+                },
+                {
+                    "text": "Departments",
+                    "icon": "building",
+                    "url": reverse("team-admin:department-list"),
+                    "permissions": ["team.add_department", "team.change_department"],
+                },
+            ],
+        }
     ]
--- cotidia/team/views/admin/department.py
+++ cotidia/team/views/admin/department.py
@@ -1,57 +1,53 @@
-from django.views.generic import ListView, DetailView
+import django_filters
+
 from django.urls import reverse
 from django.contrib import messages
-from cotidia.admin.views import AdminListView
-from django.conf import settings
+from django import forms
 
 from cotidia.account.utils import StaffPermissionRequiredMixin
-
 from cotidia.admin.views import (
     AdminListView,
     AdminDetailView,
     AdminDeleteView,
     AdminCreateView,
-    AdminUpdateView
+    AdminUpdateView,
 )
-
 from cotidia.team.models import Department
-from cotidia.team.forms.admin.department import (
-    DepartmentAddForm,
-    DepartmentUpdateForm)
+from cotidia.team.forms.admin.department import DepartmentAddForm, DepartmentUpdateForm
+
+
+class DepartmentFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        label="Name",
+        method="filter_name",
+        widget=forms.TextInput(attrs={"placeholder": "Search"}),
+    )
+
+    class Meta:
+        model = Department
+        fields = ["name"]
 
 
 class DepartmentList(AdminListView):
-    columns = (
-         ("ID", "id"),
-         ("Order", "order"),
-         ("name", "name"),
-    )
+    columns = (("ID", "id"), ("Order", "order"), ("name", "name"))
     model = Department
     template_type = "centered"
+    filterset = DepartmentFilter
 
 
 class DepartmentDetail(StaffPermissionRequiredMixin, AdminDetailView):
     model = Department
-    permission_required = 'team.change_model'
+    permission_required = "team.change_model"
     fieldsets = [
         {
-            "legend": 'Department Details',
+            "legend": "Department Details",
             "fields": [
                 [
-                    {
-                        "label": "ID",
-                        "field": "id"
-                    },
-                    {
-                        "label": "order id",
-                        "field": "order_id"
-                    },
-                    {
-                        "label": "name",
-                        "field": "name"
-                    },
+                    {"label": "ID", "field": "id"},
+                    {"label": "order id", "field": "order_id"},
+                    {"label": "name", "field": "name"},
                 ]
-            ]
+            ],
         }
     ]
 
@@ -59,48 +55,39 @@ class DepartmentDetail(StaffPermissionRequiredMixin, AdminDetailView):
 class DepartmentCreate(StaffPermissionRequiredMixin, AdminCreateView):
     model = Department
     form_class = DepartmentAddForm
-    permission_required = 'team.add_model'
+    permission_required = "team.add_model"
 
     def get_success_url(self):
-        messages.success(self.request, 'Department has been created.')
-        return reverse('team-admin:department-detail', kwargs={'pk': self.object.id})
+        messages.success(self.request, "Department has been created.")
+        return reverse("team-admin:department-detail", kwargs={"pk": self.object.id})
 
 
 class DepartmentUpdate(StaffPermissionRequiredMixin, AdminUpdateView):
     model = Department
     form_class = DepartmentUpdateForm
-    permission_required = 'team.change_model'
+    permission_required = "team.change_model"
     fieldsets = [
         {
-            "legend": 'Department Details',
+            "legend": "Department Details",
             "fields": [
                 [
-                    {
-                        "label": "ID",
-                        "field": "id"
-                    },
-                    {
-                        "label": "order id",
-                        "field": "order_id"
-                    },
-                    {
-                        "label": "name",
-                        "field": "name"
-                    },
+                    {"label": "ID", "field": "id"},
+                    {"label": "order id", "field": "order_id"},
+                    {"label": "name", "field": "name"},
                 ]
-            ]
+            ],
         }
     ]
 
     def get_success_url(self):
-        messages.success(self.request, 'Department details have been updated.')
-        return reverse('team-admin:department-detail', kwargs={'pk': self.object.id})
+        messages.success(self.request, "Department details have been updated.")
+        return reverse("team-admin:department-detail", kwargs={"pk": self.object.id})
 
 
 class DepartmentDelete(StaffPermissionRequiredMixin, AdminDeleteView):
     model = Department
-    permission_required = 'app.delete_model'
+    permission_required = "app.delete_model"
 
     def get_success_url(self):
-        messages.success(self.request, 'Department has been deleted.')
-        return reverse('team-admin:department-list')
+        messages.success(self.request, "Department has been deleted.")
+        return reverse("team-admin:department-list")
